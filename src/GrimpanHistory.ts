20b4edd9a71ce003089964df8f394ec21a44ea26
hyunS00 <optifun72@gmail.com>
2025-01-25 12:46:08 +0900

    프로토타입

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -2,10 +2,26 @@ import Grimpan from "./AbstractGrimpan.js";
 import ChromeGrimpan from "./ChromeGrimpan.js";
 import IEGrimpan from "./IEGrimpan.js";
 
+interface Clonable {
+  clone(): Clonable;
+}
+class HistoryStack extends Array implements Clonable {
+  clone(): Clonable {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
+
+  getStack() {
+    return this.stack.clone();
+  }
+
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
   }
 
   abstract initialize(): void;
